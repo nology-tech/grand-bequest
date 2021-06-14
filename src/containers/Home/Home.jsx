6db1cc7dc3bae0d1d959f4761368c88faa321f65
May Chung <maychung@mays-mbp.home>
2021-06-14 16:39:11 +0100

    added basic camera logic

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,7 +1,22 @@
-import React, { useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "../../App.scss";
+import { useHistory } from "react-router-dom";
 
-const Home = () => {
+const Home = (props) => {
+  const [hasMedia, setHasMedia] = useState(true);
+
+  useEffect(() => {
+    if (hasMedia) {
+      const history = useHistory();
+      history.push("/confirmation");
+      // minus length is the home length "home" i.e 4
+      // console.log(window.location.href);
+      // console.log(
+      //   window.location.href.slice(0, window.location.href.length - 4)
+      // );
+    }
+  }, [hasMedia]);
+
   return (
     <div className="container">
       <p>Home Test</p>
