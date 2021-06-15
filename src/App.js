f012377993dac0a1d72b1861e31362a030667c78
vicjones-dotcom <79164908+vicjones-dotcom@users.noreply.github.com>
2021-06-15 19:45:06 +0300

    created hasClicked state for dontShowButton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Modal from "../src/components/Modal/Modal";
 
 function App() {
   const [show, setShow] = useState(false);
+  const [hasClicked, setHasClicked] = useState(false);
+  useEffect(() => {
+    openModal();
+  }, []);
+  // different options for button and useEffect
 
   const openModal = () => {
-    setShow(true);
+    if (!hasClicked) {
+      setShow(true);
+    }
   };
   const closeModal = () => {
     setShow(false);
+    setHasClicked(true);
   };
+
   return (
     <div className="App">
       {!show && <button onClick={openModal}>Help</button>}
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,19 +5,21 @@ const Modal = (props) => {
   const { show, closeModal } = props;
   return (
     <>
-      <div className={show ? "modal" : "hide"}>
-        <button className="closeButton" onClick={closeModal}>
+      <div className={show ? "popUp modal" : "popUp hide"}>
+        <button className="popUp__closeButton" onClick={closeModal}>
           X
         </button>
-        <h1>Instructions</h1>
-        <p>
+        <h1 className="popUp__heading">Instructions</h1>
+        <p className="popUp__paragraph">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
           repellendus magni laborum magnam doloremque accusamus labore inventore
           sed fugit dolore? Corrupti ipsa officia quos ipsam dolore aliquam
           tempora eius asperiores?
         </p>
         <button
-          className={show ? "modal dontShowButton" : "hide dontShowButton"}
+          className={
+            show ? "modal popUp__dontShowButton" : "hide popUp__dontShowButton"
+          }
         >
           Don't show again
         </button>
