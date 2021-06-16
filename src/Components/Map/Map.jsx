ce3fd69fc71c81ae8134310cb1d938c7ca01e559
LukeTunstallll <luketunstallll@hotmail.com>
2021-06-16 17:00:08 +0100

    added click functionality

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,12 +1,54 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 
 const Map = () => {
+  const LocationMarker = () => {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here, at {position.toString()}</Popup>
+      </Marker>
+    );
+  };
+
+  const ClickMarker = () => {
+    const [position, setPosition] = useState(null);
+    useMapEvents({
+      click(e) {
+        setPosition(e.latlng);
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>{position.toString()}</Popup>
+      </Marker>
+    );
+  };
+
   return (
     <div>
-      <h1> Please Work</h1>
-
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer
+        center={[52.307639, -1.381702]}
+        zoom={7}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -14,6 +56,8 @@ const Map = () => {
         <Marker position={[51.449313, -2.58797]}>
           <Popup>Almost certainly not _Nology</Popup>
         </Marker>
+        <ClickMarker />
+        {/* <LocationMarker /> */}
       </MapContainer>
     </div>
   );
