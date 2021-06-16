e03b45108828c89aa36a1a400b657eb7ba40525f
keenzman <szaman_03@hotmail.co.uk>
2021-06-16 16:42:05 +0100

    setup firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsKrLHSZ39YkFGAZ5102GTxXdIrgGZ1nM",
+  authDomain: "grand-bequest-8ee46.firebaseapp.com",
+  projectId: "grand-bequest-8ee46",
+  storageBucket: "grand-bequest-8ee46.appspot.com",
+  messagingSenderId: "172940441785",
+  appId: "1:172940441785:web:051ee8e4c8d96b19128368",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
