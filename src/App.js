5bebca66721003260e4d37345c944af547f0a7b4
junyimn <junyimn@outlook.com>
2021-06-17 15:31:05 +0100

    added typography, inputand button styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
-import Modal from "./components/Modal";
 
 function App() {
-  const [show, setShow] = useState(false);
-
-  const openModal = () => {
-    setShow(true);
-  };
-  const closeModal = () => {
-    setShow(false);
-  };
-  return (
-    <div className="App">
-      {!show && <button onClick={openModal}>Show modal</button>}
-      <Modal closeModal={closeModal} show={show} />
-    </div>
-  );
+  return;
 }
 
 export default App;
