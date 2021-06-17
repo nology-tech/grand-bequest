dff5193c8bb7429d73e72c16a8be94940328b6d4
vicjones-dotcom <79164908+vicjones-dotcom@users.noreply.github.com>
2021-06-17 18:45:07 +0300

    Styled dont show button

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,25 +5,29 @@ const Modal = (props) => {
   const { show, handleDismiss, handleClose } = props;
   return (
     <>
-      <div className={show ? "popUp modal" : "popUp hide"}>
+      <div className={show ? "popUp popUp__modal" : "popUp hide"}>
         <button onClick={handleClose} className="popUp__closeButton">
           X
         </button>
-        <h1 className="popUp__heading">Instructions</h1>
-        <p className="popUp__paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-          repellendus magni laborum magnam doloremque accusamus labore inventore
-          sed fugit dolore? Corrupti ipsa officia quos ipsam dolore aliquam
-          tempora eius asperiores?
-        </p>
-        <button
-          onClick={handleDismiss}
-          className={
-            show ? "modal popUp__dontShowButton" : "hide popUp__dontShowButton"
-          }
-        >
-          Don't show again
-        </button>
+        <div className="popUp__container">
+          <h1 className="popUp__heading">Instructions</h1>
+          <p className="popUp__paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            repellendus magni laborum magnam doloremque accusamus labore
+            inventore sed fugit dolore? Corrupti ipsa officia quos ipsam dolore
+            aliquam tempora eius asperiores?
+          </p>
+          <button
+            onClick={handleDismiss}
+            className={
+              show
+                ? "popUp__modal popUp__dontShowButton"
+                : "hide popUp__dontShowButton"
+            }
+          >
+            Don't show again
+          </button>
+        </div>
       </div>
     </>
   );
