517ac0acccbc1c7c914a1c009a785d64a32819fa
rimichaeru <conceptual@protonmail.com>
2021-06-23 17:29:08 +0100

    Updated DB logic

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "../../App.scss";
 import { useHistory } from "react-router-dom";
-import Map from "../../components/Map/Map.jsx";
-import Overlay from "../../components/Overlay";
+import Map from "../../Components/Map/Map.jsx";
+import Overlay from "../../Components/Overlay";
 
 const Home = (props) => {
   const [hasMedia, setHasMedia] = useState(false);
@@ -41,7 +41,7 @@ const Home = (props) => {
   return (
     <div className="container">
       <Overlay></Overlay>
-      
+
       <Map
         currentLocation={props.currentLocation}
         setCurrentLocation={props.setCurrentLocation}
