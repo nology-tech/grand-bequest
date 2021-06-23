d59db32fc49d2e4fd4b2ac0b0b8dce3509bdc778
keenzman <szaman_03@hotmail.co.uk>
2021-06-23 17:16:16 +0100

    show socials on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import NavBar from "./Components/NavBar";
+import NavBar from "./components/NavBar";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./containers/Routes";
 import { useState } from "react";
 import "./App.scss";
 import "./assets/styles/main.scss";
+import Socials from "./components/Socials/Socials";
 // import {firestore} from "./firebase"
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
   return (
     <Router className="App">
       <Routes upload={upload} imgData={imgData} setImgData={setImgData} />
+      <Socials />
     </Router>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,28 @@
-import React, {} from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  
   return (
     <>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/confirmation">Confirmation</Link>
-        </li>
-        <li>
-          <Link to="/submit">Submit</Link>
-        </li>
-        <li>
-          <Link to="/details">Details</Link>
-        </li>
-        <li>
-          <Link to="/rg45tg45g5">Check Error</Link>
-        </li>
-      </ul>
-    </nav>
-      
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/confirmation">Confirmation</Link>
+          </li>
+          <li>
+            <Link to="/submit">Submit</Link>
+          </li>
+          <li>
+            <Link to="/details">Details</Link>
+          </li>
+          <li>
+            <Link to="/rg45tg45g5">Check Error</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
