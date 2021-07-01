5bb6fc25be7f84f606be59f598ecb4edfd4644f4
esmeebarr <esmeebarr@btinternet.com>
2021-07-01 16:38:21 +0100

    Added code for button

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -100,7 +100,7 @@ const Home = (props) => {
         />
       </div>
       <Socials />
-      {/* <button class="add-button">Add to home screen</button> */}
+      <button class="add-button">Add to home screen</button>
     </div>
   );
 };
