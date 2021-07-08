b0d0791cf6194abc044bd96e88c19c2a35553d7c
vicjones-dotcom <79164908+vicjones-dotcom@users.noreply.github.com>
2021-07-08 18:54:19 +0300

    fixed modal scss

--- src/Components/InfoModal/InfoModal.jsx
+++ src/Components/InfoModal/InfoModal.jsx
@@ -20,20 +20,6 @@ const InfoModal = (props) => {
       <button onClick={handleClose} className="popup__closeButton">
         X
       </button>
-      <div className="info-modal__socials-container">
-        <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.grandbequest.co.uk%2F&amp;src=sdkpreparse">
-          <FontAwesomeIcon
-            icon={["fab", "facebook-square"]}
-            className="info-modal__facebook"
-          />
-        </a>
-        <a href="https://twitter.com/intent/tweet?text=Check%20out%20this%20amazing%20old%20building,%20We%20must%20save%20it!&url=https://twitter.com/GrandBequest">
-          <FontAwesomeIcon
-            icon={["fab", "twitter-square"]}
-            className="info-modal__twitter"
-          />
-        </a>
-      </div>
       <div className="info-modal__heart">
         <FontAwesomeIcon
           icon={["far", "heart"]}
@@ -45,22 +31,18 @@ const InfoModal = (props) => {
         src="https://www.wilsons.school/history/files/image_256-687129.jpg"
         alt=""
       />
-
-      <textarea
-        className="info-modal__use-input"
-        placeholder="Potential Use"
-      ></textarea>
-      <textarea
-        className="info-modal__onwnership-input"
-        type="text"
-        placeholder="Ownership"
-      />
-      <textarea
-        className="info-modal__comments"
-        rows="3"
-        cols="10"
-        placeholder="Comments"
-      ></textarea>
+      <label htmlFor="potential use" className="info-modal__label">
+        Potential Use
+      </label>
+      <textarea className="info-modal__use-input"></textarea>
+      <label htmlFor="ownership" className="info-modal__label">
+        Ownership
+      </label>
+      <textarea className="info-modal__onwnership-input" type="text" />
+      <label htmlFor="comments" className="info-modal__label">
+        Comments
+      </label>
+      <textarea className="info-modal__comments" rows="3" cols="10"></textarea>
       <button className="primary-btn info-modal__btn">Add Comments</button>
     </div>
   );
